6205204c59c62aa6b8067e1a05a170a0f9f62864
Bhisma Dwi Wandana <bhismadw@gmail.com>
2016-05-18 09:38:37 +0700

    Add files via upload

---
+++ retail.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class retail{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br=new BufferedReader (new InputStreamReader(
+		System.in));
+		
+		String [][] barang = new String [1000][4];
+		
+		barang [0][0] = "0001"; 
+		barang [0][0] = "Shampo"; 
+		barang [0][0] = "5000"; 
+		barang [0][0] = "50"; 
+		
+		barang [0][0] = "0002"; 
+		barang [0][0] = "Snack"; 
+		barang [0][0] = "4000"; 
+		barang [0][0] = "40";
+
+		barang [0][0] = "0001"; 
+		barang [0][0] = "Shampo"; 
+		barang [0][0] = "5000"; 
+		barang [0][0] = "50";
+
+		barang [0][0] = "0001"; 
+		barang [0][0] = "Shampo"; 
+		barang [0][0] = "5000"; 
+		barang [0][0] = "50"; 
+	
+		barang [0][0] = "0001"; 
+		barang [0][0] = "Shampo"; 
+		barang [0][0] = "5000"; 
+		barang [0][0] = "50"; 
+	}a
+	}
+	}		
+}
